105cee11b8fb6ce2f10814ba04616addcbc32a29
khiemhome <khiembi161219@gmail.com>
2024-07-07 22:10:23 +0700

    new project and add template

---
+++ khiem/demo_vuejs/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Header from "./components/Header.vue";
+import ThemeSettings from "./components/ThemeSettings.vue";
+import Sidebar from "./components/Sidebar.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <div
+    class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header"
+  >
+    <Header />
+    <ThemeSettings />
+    <div class="app-main">
+      <Sidebar />
+      <div class="app-main__outer">
+        <RouterView />
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
