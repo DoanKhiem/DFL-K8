e35c25c500f4cf0698f501ad9d4f921875eb8142
phanhn438@gmail.com <phanhn438@gmail.com>
2024-07-08 13:07:47 +0700

    taoratmet

---
+++ phanh/phanhvue/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import router from './router'
+import { createApp } from 'vue'
+import App from './App.vue'
+// import Table from './Table.vue'
+
+createApp(App).use(router).mount('#app')
