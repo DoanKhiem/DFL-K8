22d165216eb26edf17f192257081e9d4d26213ef
phanhn438@gmail.com <phanhn438@gmail.com>
2024-07-08 20:39:05 +0700

    table vue

--- phanh/phanhvue/src/main.js
+++ phanh/phanhvue/src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 import router from './router'
 import { createApp } from 'vue'
 import App from './App.vue'
-// import Table from './Table.vue'
+import Table from './Table.vue'
 
 createApp(App).use(router).mount('#app')
