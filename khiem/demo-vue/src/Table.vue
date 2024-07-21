5f7ca40d5973bdbd22cf5e0ea831ade07d16ef9f
khiemhome <khiembi161219@gmail.com>
2024-07-21 11:18:19 +0700

    show data and delete

--- khiem/demo-vue/src/Table.vue
+++ khiem/demo-vue/src/Table.vue
@@ -9,29 +9,28 @@
               <thead>
                 <tr>
                   <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Sales</th>
+                  <th>Created At</th>
+                  <th>Action</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
+                <tr v-for="item, index in dataApi" :key="item.id">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.price }}</td>
+                  <td>{{ item.sale }}%</td>
+                  <td>{{ new Date(item.createdAt * 1000) }}</td>
+                  <td style="display: flex;">
+                    <div style="cursor: pointer;">
+                      <i class="fa fa-fw" aria-hidden="true" title="Copy to use edit"></i>
+                    </div>
+                    <div @click="deleteItem(item.id)" style="cursor: pointer; margin-left: 10px;">
+                      <i class="fa fa-fw" aria-hidden="true" title="Copy to use close"></i>
+                    </div>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -42,6 +41,23 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+const dataApi = ref([]);
+const callApi = async () => {
+  const response = await axios.get('https://6419ba9ef398d7d95d47d12c.mockapi.io/demo/products');
+  dataApi.value = response.data;
+  // console.log(response);
+  // console.log(dataApi.value);
+};
+callApi()
+
+const deleteItem = async (id) => {
+  await axios.delete(`https://6419ba9ef398d7d95d47d12c.mockapi.io/demo/products/${id}`);
+  callApi();
+};
+
+</script>
 
 <style lang="scss" scoped></style>
